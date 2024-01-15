4f834b3689f2d18b68b6a3a494348fcd595e3902
omisamuel <omitogunsamuel@gmail.com>
2024-01-16 00:22:54 +0100

    commit

--- infra/remote_backend_s3.tf
+++ infra/remote_backend_s3.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "dev-proj-1-jenkins-remote-s3bucket-281223"
+    bucket = "devop-project-2024"
     key    = "devops-project-1/jenkins/terraform.tfstate"
     region = "us-west-2"
   }
