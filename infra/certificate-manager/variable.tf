71111c45d41fa93e699b5473aa6f03fb52f96613
omisamuel <omitogunsamuel@gmail.com>
2024-01-17 22:43:15 +0100

    hosted-zone added

--- infra/certificate-manager/variable.tf
+++ infra/certificate-manager/variable.tf
@@ -1,8 +1,8 @@
 #variable for Certificate Manager
-variable "domain_name" {}
-variable "hosted_zone_id" {}
+#variable "domain_name" {}
+#variable "hosted_zone_id" {}
 
-/*variable "domain_name" {
+variable "domain_name" {
   description = "The domain name for the ACM certificate"
   type        = string
   default = null
@@ -12,4 +12,4 @@ variable "hosted_zone_id" {
   description = "The Route 53 hosted zone ID for the domain"
   type        = string
   default = null
-}*/
+}
