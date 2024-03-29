76e94ab2bf055923e0d84ebb6b4a87669bf4337e
omisamuel <omitogunsamuel@gmail.com>
2024-03-29 01:32:04 +0100

    ACM updated

--- infra/variables.tf
+++ infra/variables.tf
@@ -65,6 +65,11 @@ variable "ec2_user_data_install_apache" {
   default = null
 }
 
+variable domain_name {
+  domain = "omsam.de"
+}
+
+/*
 variable "domain_name" {
   description = "The domain name for the ACM certificate"
   type        = string
@@ -73,4 +78,4 @@ variable "domain_name" {
     condition     = can(regex("^[a-zA-Z0-9-]+\\.[a-zA-Z]+$", var.domain_name))
     error_message = "The domain name must be a valid domain."
   }
-}
+}*/
